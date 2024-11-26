ab655a038552d0621c261c1845240c5803ccf2ba
Kushagra <skushagra.sharma@gmail.com>
2024-11-26 10:40:55 +0530

    sepolia-test

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,14 +2,14 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
   sei,
   seiTestnet,
+  sepolia,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'Filament Web-App',
   projectId: 'a6a36fb7d48ef6daf3b987126183a32a',
   chains: [
-    seiTestnet,
-    sei
+    sepolia
   ], 
   ssr: true,
 });
